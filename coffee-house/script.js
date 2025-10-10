14740446725022082f774e73daefec048c28e0fe
Kobyopi <iakoba12345@gmail.com>
2025-10-10 15:01:19 +0400

    feat: add coffee, tea and dessert data

---
+++ coffee-house/script.js
@@ -0,0 +1,149 @@
+//Product data
+const productsData = {
+    coffee: [
+        {
+            id: 1,
+            name: 'Irish coffee',
+            description: 'Fragnant black coffee with Jameson Irish whiskey and whipped milk',
+            price: 7.00,
+            image: '../assets/coffee-1.jpg'
+        },
+        {
+            id: 2,
+            name: 'Kahlua coffee',
+            description: 'Classic coffee with milk and Kahlua liqueur under a cap of frothed milk',
+            price: 7.00,
+            image: '../assets/coffee-2.jpg'
+        },
+        {
+            id: 3,
+            name: 'Honey raf',
+            description: 'Espresso with frothed milk, cream and aromatic honey',
+            price: 5.50,
+            image: '../assets/coffee-3.jpg'
+        },
+        {
+            id: 4,
+            name: 'Ice cappuccino',
+            description: 'Cappuccino with soft thick foam in summer version with ice',
+            price: 5.00,
+            image: '../assets/coffee-4.jpg'
+        },
+        {
+            id: 5,
+            name: 'Espresso',
+            description: 'Classic black coffee',
+            price: 4.50,
+            image: '../assets/coffee-5.jpg'
+        },
+        {
+            id: 6,
+            name: 'Latte',
+            description: 'Espresso coffee with the addition of steamed milk and dense milk foam',
+            price: 5.50,
+            image: '../assets/coffee-6.jpg'
+        },
+        {
+            id: 7,
+            name: 'Latte macchiato',
+            description: 'Espresso with forthed milk and chocolate',
+            price: 5.50,
+            image: '../assets/coffee-7.jpg'
+        },
+        {
+            id: 8,
+            name: 'Coffee with cognac',
+            description: 'Fragnant black coffee with cognac and whipped cream',
+            price: 6.50,
+            image: '../assets/coffee-8.jpg'
+        }
+    ],
+    tea: [
+        {
+            id: 9,
+            name: 'Moroccan',
+            description: 'Fragnant black tea with the addition of tangerine, cinnamon, honey, lemon and mint',
+            price: 4.50,
+            image: '../assets/tea-1.png'
+        },
+        {
+            id: 10,
+            name: 'Ginger',
+            description: 'Original black tea with fresh ginger, lemon and honey',
+            price: 5.00,
+            image: '../assets/tea-2.png'
+        },
+        {
+            id: 11,
+            name: 'Cranberry',
+            description: 'Invigorating black tea with cranberry and honey',
+            price: 5.00,
+            image: '../assets/tea-3.png'
+        },
+        {
+            id: 12,
+            name: 'Sea buckthorn',
+            description: 'Toning sweet black tea with sea buckthorn, fresh thyme and cinnamon',
+            price: 5.50,
+            image: '../assets/tea-4.png'
+        }
+    ],
+    dessert: [
+        {
+            id: 13,
+            name: 'Marble cheesecake',
+            description: 'Philadelphia cheese with lemon zest on a light sponge cake and red currant jam',
+            price: 3.50,
+            image: '../assets/dessert-1.png'
+        },
+        {
+            id: 14,
+            name: 'Red velvet',
+            description: 'Layer cake with cream cheese frosting',
+            price: 4.00,
+            image: '../assets/dessert-2.png'
+        },
+        {
+            id: 15,
+            name: 'Cheesecakes',
+            description: 'Soft cottage cheese pancakes with sour cream and fresh berries and sprinkled with powdered sugar',
+            price: 4.50,
+            image: '../assets/dessert-3.png'
+        },
+        {
+            id: 16,
+            name: 'Creme brulee',
+            description: 'Delicate creamy dessert in a caramel basket with wild berries',
+            price: 4.00,
+            image: '../assets/dessert-4.png'
+        },
+        {
+            id: 17,
+            name: 'Pancakes',
+            description: 'Tender pancakes with strawberry jam and fresh strawberries',
+            price: 4.50,
+            image: '../assets/dessert-5.png'
+        },
+        {
+            id: 18,
+            name: 'Honey cake',
+            description: 'Classic honey cake with delicate custard',
+            price: 4.50,
+            image: '../assets/dessert-6.png'
+        },
+        {
+            id: 19,
+            name: 'Chocolate cake',
+            description: 'Cake with hot chocolate filling and nuts with dried apricots',
+            price: 5.50,
+            image: '../assets/dessert-7.png'
+        },
+        {
+            id: 20,
+            name: 'Black forest',
+            description: 'A combination of thin sponge cake with cherry jam and light chocolate mousse',
+            price: 6.50,
+            image: '../assets/dessert-8.png'
+        }
+    ]
+}
