d5187d09ac15636f7bb59814ce74195d7d516a5c
Kobyopi <iakoba12345@gmail.com>
2025-10-27 01:29:48 +0400

    refactor: fix assets source linkage problem

--- src/services/api.ts
+++ src/services/api.ts
@@ -15,26 +15,26 @@ const API_BASE_URL = 'https://6kt29kkeub.execute-api.eu-central-1.amazonaws.com'
 
 // Image mapping for products
 const productImages: Record<number, string> = {
-  1: 'assets/coffee-1.jpg',
-  2: 'assets/coffee-2.jpg',
-  3: 'assets/coffee-3.jpg',
-  4: 'assets/coffee-4.jpg',
-  5: 'assets/coffee-5.jpg',
-  6: 'assets/coffee-6.jpg',
-  7: 'assets/coffee-7.jpg',
-  8: 'assets/coffee-8.jpg',
-  9: 'assets/tea-1.png',
-  10: 'assets/tea-2.png',
-  11: 'assets/tea-3.png',
-  12: 'assets/tea-4.png',
-  13: 'assets/dessert-1.png',
-  14: 'assets/dessert-2.png',
-  15: 'assets/dessert-3.png',
-  16: 'assets/dessert-4.png',
-  17: 'assets/dessert-5.png',
-  18: 'assets/dessert-6.png',
-  19: 'assets/dessert-7.png',
-  20: 'assets/dessert-8.png',
+  1: './assets/coffee-1.jpg',
+  2: './assets/coffee-2.jpg',
+  3: './assets/coffee-3.jpg',
+  4: './assets/coffee-4.jpg',
+  5: './assets/coffee-5.jpg',
+  6: './assets/coffee-6.jpg',
+  7: './assets/coffee-7.jpg',
+  8: './assets/coffee-8.jpg',
+  9: './assets/tea-1.png',
+  10: './assets/tea-2.png',
+  11: './assets/tea-3.png',
+  12: './assets/tea-4.png',
+  13: './assets/dessert-1.png',
+  14: './assets/dessert-2.png',
+  15: './assets/dessert-3.png',
+  16: './assets/dessert-4.png',
+  17: './assets/dessert-5.png',
+  18: './assets/dessert-6.png',
+  19: './assets/dessert-7.png',
+  20: './assets/dessert-8.png',
 };
 
 class ApiService {
@@ -120,9 +120,9 @@ class ApiService {
 
     // Use slider-specific images for homepage carousel
     const sliderImages: Record<number, string> = {
-      1: 'assets/coffee-slider-1.png',
-      2: 'assets/coffee-slider-2.png',
-      3: 'assets/coffee-slider-3.png',
+      1: './assets/coffee-slider-1.png',
+      2: './assets/coffee-slider-2.png',
+      3: './assets/coffee-slider-3.png',
     };
 
     try {
