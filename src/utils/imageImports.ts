d5ff177333072fceefabcb6892af965dccff3153
Kobyopi <iakoba12345@gmail.com>
2025-11-12 01:27:39 +0400

    refactor: fix image import problem

--- src/utils/imageImports.ts
+++ src/utils/imageImports.ts
@@ -1,35 +1,35 @@
 // Product images mapping
 export const productImages: Record<number, string> = {
   // Coffee (1-8)
-  1: '/assets/coffee-1.jpg',
-  2: '/assets/coffee-2.jpg',
-  3: '/assets/coffee-3.jpg',
-  4: '/assets/coffee-4.jpg',
-  5: '/assets/coffee-5.jpg',
-  6: '/assets/coffee-6.jpg',
-  7: '/assets/coffee-7.jpg',
-  8: '/assets/coffee-8.jpg',
+  1: './assets/coffee-1.jpg',
+  2: './assets/coffee-2.jpg',
+  3: './assets/coffee-3.jpg',
+  4: './assets/coffee-4.jpg',
+  5: './assets/coffee-5.jpg',
+  6: './assets/coffee-6.jpg',
+  7: './assets/coffee-7.jpg',
+  8: './assets/coffee-8.jpg',
   // Tea (9-12)
-  9: '/assets/tea-1.png',
-  10: '/assets/tea-2.png',
-  11: '/assets/tea-3.png',
-  12: '/assets/tea-4.png',
+  9: './assets/tea-1.png',
+  10: './assets/tea-2.png',
+  11: './assets/tea-3.png',
+  12: './assets/tea-4.png',
   // Dessert (13-20)
-  13: '/assets/dessert-1.png',
-  14: '/assets/dessert-2.png',
-  15: '/assets/dessert-3.png',
-  16: '/assets/dessert-4.png',
-  17: '/assets/dessert-5.png',
-  18: '/assets/dessert-6.png',
-  19: '/assets/dessert-7.png',
-  20: '/assets/dessert-8.png',
+  13: './assets/dessert-1.png',
+  14: './assets/dessert-2.png',
+  15: './assets/dessert-3.png',
+  16: './assets/dessert-4.png',
+  17: './assets/dessert-5.png',
+  18: './assets/dessert-6.png',
+  19: './assets/dessert-7.png',
+  20: './assets/dessert-8.png',
 };
 
 // Slider images for homepage
 export const sliderImages: Record<number, string> = {
-  1: '/assets/coffee-slider-1.png',
-  2: '/assets/coffee-slider-2.png',
-  3: '/assets/coffee-slider-3.png',
+  1: './assets/coffee-slider-1.png',
+  2: './assets/coffee-slider-2.png',
+  3: './assets/coffee-slider-3.png',
 };
 
 
